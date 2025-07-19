5e90e68847d96a53157418c1d1ca7e692cf250f4
Vikram-singh0 <vsvikramsingh305@gmail.com>
2025-07-19 18:39:33 +0530

    completed vidoe modal file

--- modals/Videos.ts
+++ modals/Videos.ts
@@ -11,4 +11,29 @@ export interface Ivideo {
   description: string;
   videoUrl: string;
   thumbnailUrl: string;
+  controls?: boolean;
+  transformation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
 }
+const videoSchema = new Schema<Ivideo>(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    videoUrl: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+    controls: { type: Boolean, default: true },
+    transformation: {
+      height: { type: Number, default: VideoDimension.height },
+      width: { type: Number, default: VideoDimension.width },
+      quality: { type: Number, min: 1, max: 100 },
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = models?.video || model<Ivideo>("Video", videoSchema);
+
+export default Video;
